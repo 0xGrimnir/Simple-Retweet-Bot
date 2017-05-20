bb94e62a10b4d0b6126503082d89ab3be82e8c84
Tyler L. Jones <CyberVox1@Gmail.Com>
2017-05-20 12:34:30 -0400

    Added files via upload. (retweet.py & keys.py)

---
+++ retweet.py
@@ -0,0 +1,38 @@
+# Retweet bot for Twitter, using Python and Tweepy.
+# Search query via hashtag or keyword.
+# Author: Tyler L. Jones || CyberVox
+# Date: Saturday, May 20th - 2017.
+# License: MIT License.
+
+import tweepy
+from time import sleep
+# Import in your Twitter application keys, tokens, and secrets.
+# Make sure your keys.py file lives in the same directory as this .py file.
+from keys import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Where q='#example', change #example to whatever hashtag or keyword you want to search.
+# Where items(5), change 5 to the amount of retweets you want to tweet.
+# Make sure you read Twitter's rules on automation - don't spam!
+for tweet in tweepy.Cursor(api.search, q='#example').items(5):
+    try:
+        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+
+        tweet.retweet()
+        print('Retweet published successfully.')
+
+        # Where sleep(10), sleep is measured in seconds.
+        # Change 10 to amount of seconds you want to have in-between retweets.
+        # Read Twitter's rules on automation. Don't spam!
+        sleep(10)
+
+    # Some basic error handling. Will print out why retweet failed, into your terminal.
+    except tweepy.TweepError as error:
+        print('\nError. Retweet not successful. Reason: ')
+        print(error.reason)
+
+    except StopIteration:
+        break
