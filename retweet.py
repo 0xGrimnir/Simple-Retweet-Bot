ddaa252e6efe26a486ce2ea9dd32175c58ee04d2
KayleeTheMech <kaylee@posteo.de>
2018-03-22 22:14:09 +0100

    using my own key file and other hashtag

--- retweet.py
+++ retweet.py
@@ -8,7 +8,7 @@ import tweepy
 from time import sleep
 # Import in your Twitter application keys, tokens, and secrets.
 # Make sure your keys.py file lives in the same directory as this .py file.
-from keys import *
+from realkeys import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -17,7 +17,7 @@ api = tweepy.API(auth)
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.
 # Where items(5), change 5 to the amount of retweets you want to tweet.
 # Make sure you read Twitter's rules on automation - don't spam!
-for tweet in tweepy.Cursor(api.search, q='#example').items(5):
+for tweet in tweepy.Cursor(api.search, q='#TeamKeineTransfeindlichkeit').items(20):
     try:
         print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
 
