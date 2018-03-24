d212bf9fe62fb184cccef7a2dd4ab44027e911f2
KayleeTheMech <kaylee@posteo.de>
2018-03-24 13:10:16 +0100

    Fetching tweets, reversing order

--- retweet.py
+++ retweet.py
@@ -5,10 +5,12 @@
 # License: MIT License.
 
 import tweepy
+import dateparser
+
 from time import sleep
 # Import in your Twitter application keys, tokens, and secrets.
 # Make sure your keys.py file lives in the same directory as this .py file.
-from realkeys import *
+from settings import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -17,17 +19,21 @@ api = tweepy.API(auth)
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.
 # Where items(5), change 5 to the amount of retweets you want to tweet.
 # Make sure you read Twitter's rules on automation - don't spam!
-for tweet in tweepy.Cursor(api.search, q='#TeamKeineTransfeindlichkeit').items(20):
-    try:
-        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+fetchedTweets=[]
+for tweet in tweepy.Cursor(api.search, q=hashtag).items(20):
+    fetchedTweets.append(tweet)
 
-        tweet.retweet()
-        print('Retweet published successfully.')
+
+for tweet in reversed(fetchedTweets):
+    try:
+        #print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+        print(tweet.created_at)
+        #print('Retweet published successfully.')
 
         # Where sleep(10), sleep is measured in seconds.
         # Change 10 to amount of seconds you want to have in-between retweets.
         # Read Twitter's rules on automation. Don't spam!
-        sleep(10)
+
 
     # Some basic error handling. Will print out why retweet failed, into your terminal.
     except tweepy.TweepError as error:
